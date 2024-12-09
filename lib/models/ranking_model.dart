7cb8e39be238ed1a0e5ce25d24a3d7579a4eb498
mingnhi <luongbaotam2905@gmail.com>
2024-12-09 20:59:38 +0700

    fix register

--- lib/models/ranking_model.dart
+++ lib/models/ranking_model.dart
@@ -1,8 +1,9 @@
 import 'package:uuid/uuid.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RankingModel {
   final String id;
-  final String user_id;
+  final DocumentReference user_id;
   final int total_score;
   final String user_name;
   final String avatar;
